4c677579ad72b3bba73d27da04ddeae7df3c0637
DuvanMVP <158522532+DuvanMVP@users.noreply.github.com>
2024-03-12 23:00:26 -0500

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+def saludo(nombre, apellido):
+    print('Bienvenido' (nombre) (apellido))
+
+
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su apellido ahora: ')
+
+
+print('Bienvenido', nombre, apellido)
